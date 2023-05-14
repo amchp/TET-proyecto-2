9fc1fded4bf15795a63a5c6d299179b99d043384
Alejandro Mc Ewen <alejandromcewen@Angelas-MacBook-Air-2.local>
2023-05-14 18:59:12 -0500

    Adding instance_id

--- client/ABClient/GRPC/GRPCClient.py
+++ client/ABClient/GRPC/GRPCClient.py
@@ -1,7 +1,7 @@
 import grpc
 from google.protobuf.json_format import MessageToDict
 from GRPC.generated.connection import Connection_pb2, Connection_pb2_grpc
-from config import TARGET_CONNECTION, GRPC_TIMEOUT, SERVER_ADDRESS
+from config import TARGET_CONNECTION, GRPC_TIMEOUT, SERVER_ADDRESS, INSTANCE_ID
 
 
 def connect():
@@ -10,7 +10,7 @@ def connect():
         stub = Connection_pb2_grpc.ConnectionServiceStub(channel)
         response = stub.onConnect(
             Connection_pb2.onConnectRequest(
-                instance_name="MACHINE", ip_address=SERVER_ADDRESS
+                instance_name=INSTANCE_ID, ip_address=SERVER_ADDRESS
             ),
             timeout=GRPC_TIMEOUT,
         )
