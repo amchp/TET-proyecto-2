f433056f7ac935d47af5604da3861733d051f70b
Alejandro Mc Ewen <alejandromcewen@Angelas-MacBook-Air-2.local>
2023-05-15 13:57:36 -0500

    Another fix

--- ABclient.bash
+++ ABclient.bash
@@ -5,8 +5,8 @@ cd TET-proyecto-2
 sudo sh dockersetup.sh
 cd client
 sudo set -e -x
-sudo echo export PUBLIC_IP=$(ec2metadata --public-ipv4) >> .env
-sudo echo export PRIVATE_IP=$(ec2metadata --local-ipv4) >> .env
-sudo echo export INSTANCE_ID=$(ec2metadata --instance-id) >> .env
+sudo echo PUBLIC_IP=$(ec2metadata --public-ipv4) >> .env
+sudo echo PRIVATE_IP=$(ec2metadata --local-ipv4) >> .env
+sudo echo INSTANCE_ID=$(ec2metadata --instance-id) >> .env
 sudo docker compose build
 sudo docker compose up
