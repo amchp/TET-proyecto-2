de625965ddff39d8c6ec35649fd3e4b5fee41784
Alejandro Mc Ewen <alejandromcewen@Angelas-MacBook-Air-2.local>
2023-05-15 17:22:24 -0500

    Last touches

--- client/frontend/src/services/ClientService.tsx
+++ client/frontend/src/services/ClientService.tsx
@@ -4,7 +4,7 @@ const SERVER_URL = process.env.PUBLIC_IP;
 
 export async function setLoadToClient(load: number): Promise<void> {
     try {
-        await axios.post(SERVER_URL + 'load/', {
+        await axios.post(SERVER_URL + ':8000/load/', {
             load: load
         });
     } catch (error: any) {
